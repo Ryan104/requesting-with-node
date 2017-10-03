10e82aa469bf9414ca2b44e413ddd2b6a0ed9a01
Ryan <ryan.d.elliott@gmail.com>
2017-10-03 16:54:51 -0600

    add movie war

--- movieWar.js
+++ movieWar.js
@@ -1,5 +1,6 @@
 const movieWar = (movieCounts) => {
 	console.log(movieCounts);
+	// this module should take the total number of results found for each movie and return which movie had the most as the winner
 }
 
 module.exports = movieWar;
